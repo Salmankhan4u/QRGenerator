eb3bf50352956239bd8ca34376ab86e4db986c49
Salmankhan4u <salman3172@gmail.com>
2023-10-02 15:16:45 +0530

    Add files via upload

---
+++ BarcodeRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZatcaApi
+{
+    public class BarcodeRequest
+    {
+        public string SellerName { get; set; }
+        public string TaxNo { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public decimal TaxAmount { get; set; }
+    }
+}
